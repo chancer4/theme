7498557defaa1f3801bdf1b91009dce82e4419d0
Chance Rhodes <chance.rhodes5@gmail.com>
2015-12-22 15:33:43 -0500

    Added modal
    also added div to stop the scrolling of the embedded map

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-$(document).ready(function($){
+$(document).ready(function(){
 
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-	ctx.font = "15px Georgia";
-	ctx.fillStyle= "grey";
-	ctx.textBaseLine = "top";
-	ctx.fillText("CSS",25,25);
-	ctx.moveTo(25,50);
-	ctx.lineTo(200,50);
-	ctx.stroke();
+	$(".progress-bar-success").animate({
+	    width: "35%"
+	}, 2500);
 
 });
