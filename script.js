aeefe027b6126cb60277b24f42d09ce0f54d59d4
Chance Rhodes <chance.rhodes5@gmail.com>
2016-01-15 10:53:24 -0500

    fixed a syntax error

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 	  $("#owl-demo").owlCarousel({
       autoPlay : 3000,
-      stopOnHover : true
-      navigation : true, // Show next and prev buttons
+      stopOnHover : true,
+      navigation : false, // Show next and prev buttons
       slideSpeed : 300,
       paginationSpeed : 400,
       singleItem:true
